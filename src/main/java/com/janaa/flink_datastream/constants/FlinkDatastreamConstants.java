97efca6eea62ae90f2ba43df1f973c9d1fd8f66e
janaarthan1022 <janaarthanarch@gmail.com>
2025-01-11 22:19:05 +0530

    flink-constants class

---
+++ src/main/java/com/janaa/flink_datastream/constants/FlinkDatastreamConstants.java
@@ -0,0 +1,11 @@
+package com.janaa.flink_datastream.constants;
+
+public class FlinkDatastreamConstants {
+
+	public static final String FLINK_JOB_NAME = "Flink Kafka Data Streaming Job";
+	public static final String KAFKA_TOPIC_SOURCE = "kafka.topic.source";
+	public static final String KAFKA_TOPIC_ODD = "kafka.topic.odd";
+	public static final String KAFKA_TOPIC_EVEN = "kafka.topic.even";
+	public static final String OUTPUT_FILE_EVEN = "output.file.even";
+	public static final String OUTPUT_FILE_ODD = "output.file.odd";
+}
